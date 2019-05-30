755077dc26c4478d02ad2f667621d678220c72ce
theimperium20 <survesh.in.heaven@gmail.com>
2019-05-30 15:49:36 +0530

    Update settings.py

--- bucketlist/settings.py
+++ bucketlist/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+#-$7v6vqw-y8163y82z1%)5jk@dzj_ykaf@r0&#38ms7da*gh'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bucketlist-restapi.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['YOUR_HEROKU_APP','localhost', '127.0.0.1']
 
 
 # Application definition
